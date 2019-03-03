7f6c7d02587eda77539807c8b233618b26ed0679
Zane Campbell <zanegitinit@gmail.com>
2019-03-03 11:52:23 -0800

    setup request to darksky

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const request = require('request');
 
 const url = 'https://api.darksky.net/forecast/77bb2bfa7a3d8d204021a7de87ff8f1e/37.8267,-122.4233';
 
-request({ url }, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.currently);
+request({ url: url, json: true }, (error, response) => {
+    const temp = response.body.currently.temperature
+    const rainChance = response.body.currently.precipProbability
+    console.log(`${response.body.daily.data[0].summary} It is current ${temp} degrees out. There is a ${rainChance}% chance of rain.`);
 });
