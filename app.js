b9d93ef5dfb9a0e631e2e3c29d3b4c257583df8c
Zane Campbell <zanegitinit@gmail.com>
2019-03-05 17:29:34 -0800

    setup geocode request callback

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const geocode = require('./utils/geocode.js');
 
 const url = 'https://api.darksky.net/forecast/77bb2bfa7a3d8d204021a7de87ff8f1e/37.8267,-122.4233';
 
@@ -14,18 +15,23 @@ const url = 'https://api.darksky.net/forecast/77bb2bfa7a3d8d204021a7de87ff8f1e/3
 //     }
 // });
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiemFuZWNhbXBiZWxsIiwiYSI6ImNqc3VzZjZhZDFreTQzeXBhcmNxb2Z2bmsifQ.YSsGUrJLZIqfqasi1Dlz7Q&limit=1'
+// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiemFuZWNhbXBiZWxsIiwiYSI6ImNqc3VzZjZhZDFreTQzeXBhcmNxb2Z2bmsifQ.YSsGUrJLZIqfqasi1Dlz7Q&limit=1'
 
-request({ url: geocodeURL, json: true }, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to location service!')
-    } else if (response.body.features.length === 0) {
-        console.log('Unable to find location!')
-    } else {
-        const latitude = response.body.features[0].center[0]
-        const longitude = response.body.features[0].center[1]
-        console.log(latitude, longitude);
-    }
+// request({ url: geocodeURL, json: true }, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to location service!')
+//     } else if (response.body.features.length === 0) {
+//         console.log('Unable to find location!')
+//     } else {
+//         const latitude = response.body.features[0].center[0]
+//         const longitude = response.body.features[0].center[1]
+//         console.log(latitude, longitude);
+//     }
+// });
+
+geocode('Seattle', (error, data) => {
+    console.log('Error', error);
+    console.log('Data', data);
 });
 
 
