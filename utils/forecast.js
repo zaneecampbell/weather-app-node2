3e30f62a275de7119e72856149f953bc33ba47e8
Zane Campbell <zanegitinit@gmail.com>
2019-03-08 07:29:04 -0800

    refactored with destructuring

--- utils/forecast.js
+++ utils/forecast.js
@@ -3,15 +3,15 @@ const request = require('request');
 const forecast = (latitude, longitude, callback) => {
     const url = `https://api.darksky.net/forecast/77bb2bfa7a3d8d204021a7de87ff8f1e/${latitude},${longitude}`
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to forecast services!', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location. Try another search.', undefined)
         } else {
-            const temp = response.body.currently.temperature
-            const rainChance = response.body.currently.precipProbability
-            const summary = response.body.daily.data[0].summary
+            const temp = body.currently.temperature
+            const rainChance = body.currently.precipProbability
+            const summary = body.daily.data[0].summary
             callback(undefined, `${summary} It is current ${temp} degrees out. There is a ${rainChance}% chance of rain.`)
         }
     })   
